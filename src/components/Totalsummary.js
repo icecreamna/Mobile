2cdbadff5ab1ea89adba8373f65805525508115e
I don't know <kittabeth2547@gmail.com>
2025-04-27 23:45:36 +0700

    Update Totalsummary.js

--- src/components/Totalsummary.js
+++ src/components/Totalsummary.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Text, View, StyleSheet } from 'react-native'
 
 const TotalPrice = ({totalCost,darkmode}) => {
-    // Calculate the total price for unpurchased items
     return (
         <View>
             <Text style ={darkmode ? styles.darkTotalText : styles.totalText}>Total Price (Unpurchased) : {totalCost} </Text>
